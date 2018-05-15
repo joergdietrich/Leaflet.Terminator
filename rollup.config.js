409ec551230f1ee6a812483559d3acd191453d4b
caub <cyril.auburtin@gmail.com>
2018-05-15 14:59:03 +0200

    update deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,12 @@
 export default {
-  name: 'L.terminator',
-  input: 'index.js',
-  output: {
-    file: 'L.Terminator.js',
-    format: 'umd'
-  },
-  external: ['leaflet'],
-  globals: {
-    'leaflet': 'L'
-  }
+	input: 'index.js',
+	output: {
+		format: 'umd',
+		file: 'L.Terminator.js',
+		name: 'L.terminator',
+		globals: {
+			'leaflet': 'L'
+		}
+	},
+	external: ['leaflet']
 }
